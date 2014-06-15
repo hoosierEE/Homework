5a69ab7a1ade0ca7e2971a5bc226e24cebee048e
hoosierEE <ashroyer@gmail.com>
2014-06-14 22:30:46 -0400

    latest from laptop

--- Numeric/FizzBuzz/templated/templated.cpp
+++ Numeric/FizzBuzz/templated/templated.cpp
@@ -3,39 +3,18 @@
 
 using namespace std;
 
-template <typename T>
-T modulator (T numerator, T divisor) {
-  return !(numerator % divisor);
+template <typename T> string
+fb(T num) {
+  string answer = "";
+  string stringifiedNum = to_string(num);
+  return stringifiedNum;
 }
-
-template <int n> struct
-fb {
-  static const int value = fb<n >> 1>::value;
-};
-
-//template <int T> class
-//modu {
-//  modu() { cout << T << endl; };
-//  ~modu() {};
-//};
-//template<0> class
-//modu {
-//  modu() { cout << "special" << endl; };
-//  ~modu() {};
-//}
 
 int main() {
 
   int startNumber = 100;
 
-  while (startNumber--){
-    if (modulator(startNumber, 15))
-      cout << startNumber << " fizzbuzz" << endl;
-    else if (modulator(startNumber, 5))
-      cout << startNumber << " buzz" << endl;
-    else if (modulator(startNumber, 3))
-      cout << startNumber << " fizz" << endl;
-    else cout << startNumber << endl;
-  }
-
+  do {
+    cout << fb(startNumber) << endl;
+  } while (--startNumber);
 }
