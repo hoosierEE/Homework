217bf273b3fa67ed40df53f0f6e853be7af12618
hoosierEE <ashroyer@gmail.com>
2014-06-22 16:10:06 -0400

    reading one file and printing its contents

--- FileIO/main.cpp
+++ FileIO/main.cpp
@@ -1,7 +1,21 @@
+// a c++ file to process the results of a data logging run of my oscilloscope./*{{{*/
+// the scope saves its run as a bunch of csv files, to use this program, put the
+// scope files in a directory "raw" and position this program so it can access raw/[files].
+// that is, the to-level structure should look like this:
+//
+// some-dir/
+//   this_program
+//   raw/
+//     file1
+//     file2
+//     ...
+//     filen/*}}}*/
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <vector>
 
 int main(){
   std::ifstream raw;
@@ -9,7 +23,11 @@ int main(){
   raw.open("raw/test1.csv", std::ios::in);
   results.open("processed/test1processed.txt", std::ios::out);
 
-  //std::cout << type
+  std::string line;
+
+  while(!getline(raw,line).eof()){
+    std::cout << line << std::endl;
+  }
 
   raw.close();
   results.close();
