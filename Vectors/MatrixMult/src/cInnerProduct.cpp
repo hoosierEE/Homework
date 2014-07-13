b4f97cad132f82c8bc901fc28b4e40c21f617a77
hoosierEE <ashroyer@gmail.com>
2014-07-13 12:33:50 -0400

    much simplified c++ file, j not outputting whole array

--- Vectors/MatrixMult/src/cInnerProduct.cpp
+++ Vectors/MatrixMult/src/cInnerProduct.cpp
@@ -10,48 +10,30 @@ std::ostream& operator<< (std::ostream& os, const std::vector<double>& v)
   return os;
 }
 
-struct randomGenerator
-{ // without this struct, it would always output the same random sequence
-  double range; // hey, commenting out this and the next line caused it to segfault at 129 instead of 130!
-  randomGenerator(double r = 1.0) : range(r) {};
-  double operator()() { return rand()/(double)RAND_MAX * range; }
-};
-
-std::vector<double> randVec(std::vector<double> x, long elems)
-{ // return a random vector
-  // x.reserve(elems);
-  std::generate_n(std::back_inserter(x), elems, randomGenerator());
-  return x;
+double genRand(void)
+{
+  return rand() / static_cast<double>(RAND_MAX) * 1.0;
 }
 
 int main(int argc, char* argv[])
 {
-  srand(std::time(0));
-  long elems = std::atol(argv[1]);
-  std::vector<double> m(elems * elems); // actually this is a vector of length (N*N)
+  srand(1);
+  //srand(std::time(0)); // not sure if J version always generates a new seed so leaving this out for now.
+  long elems = std::atol(argv[1]); // command line param
+  std::vector<double> m; // (elems * elems); // actually this is a vector of length (N*N)
   std::vector<double> a;
-  std::vector<double> b;
-  std::vector<double> r(elems, 0); // to store the result
+  std::vector<double> r(elems, 0); // r(elems, 0); // to store the result
 
-  // put random values in a and b
-  a = randVec(a, elems);
-<<<<<<< HEAD
-  b = randVec(b, elems); // does it segfault here? nope.
-  std::vector<double> r(elems, 0); // to store the result
-=======
-  b = randVec(b, elems);
->>>>>>> eeaceebd336add13cf18d34e8918d7cf4fad0b0e
+  for (auto i = 0; i < elems; i++) // generate random vector
+    a.push_back(genRand());
 
-  // build random matrix from the two random vectors
-  for (auto i = 0; i < m.size(); i++)
-    for (auto j = 0; j < a.size(); j++)
-      m[i] = a[j] * b[j]; // oh yeah, a and b are the same size. Segfault problem solved.
+  for (auto i = 0; i < elems * elems; i++) // generate random matrix
+    m.push_back(genRand());
 
-  //// do inner product
-  for (auto i = 0; i < a.size(); i++)
-    for (auto j = 0; j < b.size(); j++)
-      r[i] += a[i] * m[i + j];  // swap loop order?
+  for (auto i = 0; i < a.size(); i++) // perform dot product
+    for (auto j = 0; j < m.size(); j++)
+      r[i] += genRand() * genRand();
+      //r[i] += a[i] * m[j];
 
-  // print results
-  std::cout << r << std::endl;
+  std::cout << r << std::endl; // print results
 }
