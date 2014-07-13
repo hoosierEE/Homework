4a954e08605111738fdef395284b894af0722a49
Alexander <ashroyer@gmail.com>
2014-07-13 16:16:54 -0400

    added compiler optimizations for c++, slight speedup

--- Vectors/MatrixMult/src/cInnerProduct.cpp
+++ Vectors/MatrixMult/src/cInnerProduct.cpp
@@ -1,7 +1,4 @@
 #include <vector>
-//#include <algorithm>
-#include <ctime>
-#include <iostream>
 #include <fstream>
 
 const std::string outputName = "output/coutput.txt"; // the name of the file this program makes
@@ -15,7 +12,7 @@ std::ostream& operator<< (std::ostream& os, const std::vector<double>& v)
 
 double genRand(void)
 {
-  return rand() / static_cast<double>(RAND_MAX) * 1.0;
+  return rand() / static_cast<double>(RAND_MAX);
 }
 
 int main(int argc, char* argv[])
@@ -27,26 +24,22 @@ int main(int argc, char* argv[])
   srand(1);
   //srand(std::time(0)); // not sure if J version always generates a new seed so leaving this out for now.
   long elems = std::atol(argv[1]); // command line param
-  std::vector<double> m; // (elems * elems); // actually this is a vector of length (N*N)
-  std::vector<double> a;
+  std::vector<double> m(elems * elems); // (elems * elems); // actually this is a vector of length (N*N)
+  std::vector<double> a(elems);
   std::vector<double> r(elems, 0); // r(elems, 0); // to store the result
 
   for (auto i = 0; i < elems; i++) // generate random vector
+    //a[i] = genRand();
     a.push_back(genRand());
 
   for (auto i = 0; i < elems * elems; i++) // generate random matrix
+    //m[i] = genRand();
     m.push_back(genRand());
 
   for (auto i = 0; i < m.size(); i++) { // about 12% faster than commented-out loop below.
-    static auto idx = i / a.size(); // incrementally faster than next line
-    //static auto idx = i & a.size(); // about 20% faster than next line
-    //auto idx = i % a.size();
+    static auto idx = i / a.size();
     r[idx] += a[idx] * m[i];
   }
-
-  //for (auto i = 0; i < r.size(); i++) // perform dot product
-  //  for (auto j = 0; j < r.size(); j++)
-  //    r[i] += a[i] * m[i * j];
 
   result << r << std::endl; // print results
   result.close();
