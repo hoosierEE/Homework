88abbc0aa5950d4f894b11d32d90f34be1802cd3
hoosierEE <ashroyer@gmail.com>
2014-07-13 10:42:48 -0400

    cleaned up puzzles/inverter

--- Vectors/Puzzles/inverter.cpp
+++ Vectors/Puzzles/inverter.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <math.h>
 
 // an interview question:
 // "design a function such that f(f(n)) == -n, where n is a 32 bit signed int,
 // and which does not use complex numbers"
+//
+// are globals or external state considered "complex numbers"?
+// a philosophical question.
 
 int minusItself(int n)
-{ // are globals or external state considered "complex numbers"?
-  // a very philosophical question.
+{
   static bool hasCalledItself = false;
   if (hasCalledItself) {
     hasCalledItself = false;
@@ -20,16 +21,26 @@ int minusItself(int n)
 double changesItself(double n)
 { // uses doubles instead of ints. again, this is "extra info"
   // and would fit a liberal definition of a complex number.
-  if (n == int(n)) // first time called
-    return n + 0.1;
-  else
-    return int(-n + 0.1);
+  return (n == static_cast<int>(n)) ? n + 0.1 : static_cast<int>(-n + 0.1);
 }
 
 int main()
 {
-  for (int i = -3; i < 4; i++)
+  for (int i = -1; i < 2; i++)
     std::cout << "minusItself(minusItself(" << i << "): " << minusItself(minusItself(i)) << std::endl;
-  for (int i = -3; i < 4; i++)
+
+  std::cout << std::endl;
+
+  for (int i = -1; i < 2; i++)
     std::cout << "changesItself(changesItself(" << i << "): " << changesItself(changesItself(i)) << std::endl;
 }
+
+// output:
+//
+// minusItself(minusItself(-1): 1
+// minusItself(minusItself(0): 0
+// minusItself(minusItself(1): -1
+//
+// changesItself(changesItself(-1): 1
+// changesItself(changesItself(0): 0
+// changesItself(changesItself(1): -1
